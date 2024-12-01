7a28e830bfc9f9f5e86853570f3ad83096a3b9cb
Shubham <yadav105shubham@gmail.com>
2024-12-01 19:11:12 +0530

    add pubsub basic chat

---
+++ pubsub-basic-chat/index.js
@@ -0,0 +1,30 @@
+import http from 'node:http';
+import serveHandler from 'serve-handler';
+import ws, { WebSocketServer } from 'ws';
+import { Redis } from 'ioredis';
+
+const redisPub = new Redis();
+const redisSub = new Redis();
+
+const server = http.createServer((req, res) => {
+  serveHandler(req, res, {public: "www"});
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", client => {
+  console.log("Client connected");
+  client.on("message", msg => {
+    broadcast(msg);
+  });
+})
+
+function broadcast(msg) {
+  for( const client of wss.clients){
+    if(client.readyState === ws.OPEN) {
+      client.send(msg);
+    }
+  }
+}
+
+server.listen(process.argv[2] || 8080);
